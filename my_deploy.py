9252ce4549d619aa31f20d9115efa79e9763e721
Aviral Meharishi <aviralmeharishi@gmail.com>
2025-03-04 12:03:12 +0530

    Update my_deploy.py

--- my_deploy.py
+++ my_deploy.py
@@ -72,11 +72,11 @@ def main():
     with tab1:
         st.subheader("🔍 Check Your Mental Health")
         
-        ag = st.slider('Enter Your Age', 10, 100, 25)
+        ag = st.slider('Enter Your Age', 10, 100)
         gen = 0 if st.selectbox('Gender', ['Female', 'Male']) == 'Female' else 1
         wp = 0 if st.selectbox('Are you a?', ['Student', 'Working Professional']) == 'Student' else 1
-        apwp = st.slider('Work/Academic Pressure (1-5)', 1, 5, 3)
-        js = st.slider('Job/Study Satisfaction (1-5)', 1, 5, 3)
+        apwp = st.slider('Work/Academic Pressure (1-5)', 1, 5)
+        js = st.slider('Job/Study Satisfaction (1-5)', 1, 5)
         cgpa = st.number_input('Your CGPA (0 if Working)', 0.0, 10.0, step=0.1)
         
         sd = {'Less than 5 hours': 0, '5-6 hours': 1, '7-8 hours': 2, 'More than 8 hours': 3}[st.selectbox('Sleep Schedule', ['Less than 5 hours', '5-6 hours', '7-8 hours', 'More than 8 hours'])]
@@ -84,7 +84,7 @@ def main():
         suc = 0 if st.selectbox('Ever had Suicidal Thoughts?', ['No', 'Yes']) == 'No' else 1
         fhmi = 0 if st.selectbox('Family History of Mental Illness?', ['No', 'Yes']) == 'No' else 1
         finstress = {'Least': 1, 'Slightly': 2, 'Moderate': 3, 'High': 4, 'Severe': 5}[st.selectbox('Financial Stress?', ['Severe', 'High', 'Moderate', 'Slightly', 'Least'])]
-        workstudyhr = st.slider('Daily Work/Study Hours', 0, 24, 8)
+        workstudyhr = st.slider('Daily Work/Study Hours', 0, 24)
         deg = {'Schooling': 0, 'Undergraduate': 1, 'Postgraduate': 2, 'PhD': 3}[st.selectbox('Education Level', ['Schooling', 'Undergraduate', 'Postgraduate', 'PhD'])]
 
         input_list = [ag, gen, wp, apwp, js, cgpa, sd, dt, suc, fhmi, finstress, workstudyhr, deg]
